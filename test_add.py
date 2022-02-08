c86232d313aceeeca2b885ee8abfdeaef59f1b25
lilimart <lilianamartinezvillatoro@hotmail.es>
2022-02-08 17:59:46 -0600

    init files

---
+++ test_add.py
@@ -0,0 +1,13 @@
+from add import add, addNumberList
+
+
+def test_add():
+    result = add(1, 2)
+    assert result == 3
+
+
+def test_addNumberList_pepito():
+    result = addNumberList([])
+    assert result == 0
+    result = addNumberList([1, 2])
+    assert result == 3
